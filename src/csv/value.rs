81aecbde0abd3eaa6a0fb9467e1a735837da685f
PeterAkaJohn <pierpaolo.iannone93@gmail.com>
2024-09-22 21:17:04 +0200

    feat(csv): implement TryFrom for value

--- src/csv/value.rs
+++ src/csv/value.rs
@@ -1,3 +1,5 @@
+use crate::ParsingError;
+
 use super::token::{Number, Token};
 
 #[derive(Debug)]
@@ -25,6 +27,58 @@ impl From<&Token> for Value {
             Token::Number(number) => Value::Number(number.clone()),
             Token::Null => Value::Null,
             Token::Boolean(boolean) => Value::Boolean(*boolean),
+        }
+    }
+}
+
+impl TryFrom<Value> for String {
+    type Error = ParsingError;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::String(string) => Ok(string),
+            _ => Err(ParsingError {
+                message: format!("value {:?} is not a string", value),
+            }),
+        }
+    }
+}
+
+impl TryFrom<Value> for i64 {
+    type Error = ParsingError;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Number(Number::Int(number)) => Ok(number),
+            _ => Err(ParsingError {
+                message: format!("value {:?} is not an integer", value),
+            }),
+        }
+    }
+}
+
+impl TryFrom<Value> for f64 {
+    type Error = ParsingError;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Number(Number::Float(number)) => Ok(number),
+            _ => Err(ParsingError {
+                message: format!("value {:?} is not a float", value),
+            }),
+        }
+    }
+}
+
+impl TryFrom<Value> for bool {
+    type Error = ParsingError;
+
+    fn try_from(value: Value) -> Result<Self, Self::Error> {
+        match value {
+            Value::Boolean(boolean) => Ok(boolean),
+            _ => Err(ParsingError {
+                message: format!("value {:?} is not a boolean", value),
+            }),
         }
     }
 }
